03bd178dd478902886e6ed4554bb59681d56ca71
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-11-26 15:54:05 -0500

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -30,6 +30,7 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>bdf"] ={ "<cmd>tabnew<cr>", desc = "new tab"},
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },

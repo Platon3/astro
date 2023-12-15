da387126eb02ca1a5a0c99538062693e85f05901
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 18:17:18 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,10 +4,10 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
- { import = "astrocommunity.colorscheme.catppuccin" },
+ { import = "astrocommunity.colorschem.tokyonight" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.completion.copilot-lua" },
+
    
 
 }

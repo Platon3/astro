4ea93ba691145290bf39db7778382c4ded098a3a
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 16:14:16 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,6 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
  -- { import = "astrocommunity.colorscheme.elflord" },
+  { import = "astrocommunity.pack.json" },
 
 }

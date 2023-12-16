1138f1dea677c2b0f4b07ddbded5df060f3b0ccd
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 19:10:18 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -38,6 +38,19 @@ return {
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
         },
+        {
+        setup_handlers = {
+      -- add custom handler
+      jdtls = function(_, opts)
+        vim.api.nvim_create_autocmd("Filetype", {
+          pattern = "java", -- autocmd to start jdtls
+          callback = function()
+            if opts.root_dir and opts.root_dir ~= "" then require("jdtls").start_or_attach(opts) end
+          end,
+        })
+      end
+    },
+      },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code

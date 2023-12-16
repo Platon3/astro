b3e1a2e3eebfc045e630c0cde41cdb8aeb273fba
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 23:11:43 -0500

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,5 +9,17 @@ return {
      require("lsp_signature").setup()
      end,
    },
+  {
+  opts = {
+  size = 10,
+  open_mapping = [[<F7>]],
+  shading_factor = 2,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    highlights = { border = "Normal", background = "Normal" },
+  },
+},
+  }.
    
 }

912ac982a168558ef22eeaef812ad6f8ad6ec51b
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 23:10:22 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -82,5 +82,15 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    opts = {
+  size = 10,
+  open_mapping = [[<F7>]],
+  shading_factor = 2,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    highlights = { border = "Normal", background = "Normal" },
+  },
+}
   end,
 }

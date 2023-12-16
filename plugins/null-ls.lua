97e349b6859fe35e8f38b13133269b3b367bdecc
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 23:04:42 -0500

    Update null-ls.lua

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,9 +10,8 @@ return {
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-       null_ls.builtins.formatting.prettier,
-       null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.pint,
+      -- null_ls.builtins.formatting.prettier,
+      
     }
     return config -- return final config table
   end,

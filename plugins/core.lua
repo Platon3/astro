39917722e59a0e9c9dbf827399fdf1af2a1ba773
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-15 22:43:44 -0500

    Update core.lua

--- plugins/core.lua
+++ plugins/core.lua
@@ -74,8 +74,7 @@ return {
   --     }, { mode = "n", prefix = "<leader>" })
   --   end,
   -- },
-  {
-    "mfussenegger/nvim-jdtls",
+  "mfussenegger/nvim-jdtls",
     ft = { "java" },
     init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "jdtls") end,
     dependencies = { "williamboman/mason-lspconfig.nvim" },
@@ -184,36 +183,7 @@ return {
           require("jdtls").setup_dap { hotcodereplace = "auto" }
           require("astronvim.utils.lsp").on_attach(client, bufnr)
         end,
-      }
-
-      -- TODO: add overwrite for on_attach
-
-      -- ensure that table is valid
-      if not opts then opts = {} end
-
-      -- extend the current table with the defaults keeping options in the user opts
-      -- this allows users to pass opts through an opts table in community.lua
-      opts = vim.tbl_deep_extend("keep", opts, defaults)
-
-      -- send opts to config
-      return opts
-    end,
-    config = function(_, opts)
-      -- setup autocmd on filetype detect java
-      vim.api.nvim_create_autocmd("Filetype", {
-        pattern = "java", -- autocmd to start jdtls
-        callback = function()
-          if opts.root_dir and opts.root_dir ~= "" then
-            require("jdtls").start_or_attach(opts)
-            -- require('jdtls.dap').setup_dap_main_class_configs()
-          else
-            require("astronvim.utils").notify(
-              "jdtls: root_dir not found. Please specify a root marker",
-              vim.log.levels.ERROR
-            )
-          end
-        end,
-      })
+      },
       
  
    

150cdd922d962929446bf316192015d1c37a94c4
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-18 18:41:52 -0500

    Update treesitter.lua

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -6,7 +6,8 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
         "angular",
-       
+       "lua", 
+        "luap",
     })
   end,
 }

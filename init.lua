e095c963f3b8699db4f5d665bb5ab376384aa7ee
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-18 21:55:54 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -33,15 +33,15 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+           "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+           "python",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
+         "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -50,7 +50,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+       "pyright"
     },
   },
  

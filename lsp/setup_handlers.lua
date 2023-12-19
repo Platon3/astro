a77bbd90a15520f039273079dabde9303b686957
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-19 15:22:25 -0500

    Update setup_handlers.lua

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,12 +1,45 @@
 return 
 {
-  jdtls = function(_, opts)
-        vim.api.nvim_create_autocmd("Filetype", {
-          pattern = "java", -- autocmd to start jdtls
-          callback = function()
-            if opts.root_dir and opts.root_dir ~= "" then require("jdtls").start_or_attach(opts) end
-          end,
-        })
-      end,
+  -- TODO: add overwrite for on_attach
+
+      -- ensure that table is valid
+      if not opts then opts = {} end
+
+      -- extend the current table with the defaults keeping options in the user opts
+      -- this allows users to pass opts through an opts table in community.lua
+      opts = vim.tbl_deep_extend("keep", opts, defaults)
+
+      -- send opts to config
+      return opts
+    end,
+    config = function(_, opts)
+      -- setup autocmd on filetype detect java
+      vim.api.nvim_create_autocmd("Filetype", {
+        pattern = "java", -- autocmd to start jdtls
+        callback = function()
+          if opts.root_dir and opts.root_dir ~= "" then
+            require("jdtls").start_or_attach(opts)
+            -- require('jdtls.dap').setup_dap_main_class_configs()
+          else
+            require("astronvim.utils").notify(
+              "jdtls: root_dir not found. Please specify a root marker",
+              vim.log.levels.ERROR
+            )
+          end
+        end,
+      })
+      -- create autocmd to load main class configs on LspAttach.
+      -- This ensures that the LSP is fully attached.
+      -- See https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration
+      vim.api.nvim_create_autocmd("LspAttach", {
+        pattern = "*.java",
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          -- ensure that only the jdtls client is activated
+          if client.name == "jdtls" then require("jdtls.dap").setup_dap_main_class_configs() end
+        end,
+      })
+    end,
+  
   
 }

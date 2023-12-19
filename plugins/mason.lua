3220602fbf399d671954b5ca87b5020b37a2dc3f
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-18 20:09:58 -0500

    Update mason.lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -43,15 +43,5 @@ return {
       })
     end,
   },
-   opts = {
-        handlers = {
-          python = function(source_name)
-            local dap = require "dap"
-            dap.adapters.python = {
-              type = "executable",
-              command = "/usr/bin/python3",
-              args = {
-                "-m",
-                "debugpy.adapter",
-              },
+   
 }

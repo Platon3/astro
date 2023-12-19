b14ff7e8b4ea317fb570d04cfe061aaa78f4a656
Platon3 <118079978+Platon3@users.noreply.github.com>
2023-12-19 00:36:37 -0500

    Update mason.lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -9,8 +9,7 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "lua_ls",
          "angularls",
-         "jdtls", 
-          "lemminx",
+         
           
           
           
@@ -25,7 +24,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
-         "clang_format",
+        
           
          
           
@@ -38,9 +37,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        --"python",
-          "javadbg",
-          "javatest",
+        "python",
+         
          
           
       })
